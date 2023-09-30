a55bcb36189fac572c535af3fa5c248791069fab
StanislavMolchanovStudentKarazinUa <144453439+StanislavMolchanovStudentKarazinUa@users.noreply.github.com>
2023-09-30 20:51:48 +0200

    Made practice3.cpp (without header file)

--- practice3.cpp
+++ practice3.cpp
@@ -59,7 +59,19 @@ Footbolist SetFootbolist() {
     cin >> (&footbolist)->goal_count;
     return footbolist;
 }
-
+Footbolist SetOtherFootbolist() {
+    Footbolist footbolist;
+    cout << "New footbolist:\nFootbolist's surname: ";
+    cin >> (&footbolist)->surname;
+    (&footbolist)->role = SetRole();
+    cout << "Footbolist's age: ";
+    cin >> (&footbolist)->age;
+    cout << "Footbolist's games count: ";
+    cin >> (&footbolist)->games_count;
+    cout << "Footbolist's goal count: ";
+    cin >> (&footbolist)->goal_count;
+    return footbolist;
+}
 struct tnode {
     Footbolist footbolist;
     tnode* next = NULL;
@@ -125,6 +137,65 @@ void formFIFO_2(int n, pnode * p) {
 }
 
 /////////////////////////////////////////////////////////////////////
+bool deleteNode(pnode * head, pnode p) {
+    pnode cur = *head;
+    if (*head == p) {
+        if (*head == NULL) return false;
+        if ((*head)->next == NULL) {
+            delete *head;
+            *head = NULL;
+            return true;
+        }
+        *head = (*head)->next;
+        delete cur->next;
+        return true;
+    }
+    while (cur->next != NULL) {
+        if (cur->next == p) {
+            cur->next = p->next;
+            delete p;
+            return true;
+        }
+        cur = cur->next;
+    }
+    return false;
+}
+bool insertBefore(pnode * head, pnode p, Footbolist f) {
+    pnode cur = *head;
+    if (cur == p) {
+        if (p == NULL) return false;
+        *head = new tnode;
+        (*head)->footbolist = f;
+        (*head)->next = p;
+        return true;
+    }
+    while (cur->next != NULL) {
+        if (cur->next == p) {
+            cur->next = new tnode;
+            cur->next->footbolist = f;
+            cur->next->next = p;
+            return true;
+        }
+        cur = cur->next;
+    }
+    return false;
+}
+bool insertAfter(pnode * head, pnode p, Footbolist f) {
+    pnode cur = *head;
+    if (p == NULL) return false;
+    while (cur != NULL) {
+        if (cur == p) {
+            cur = cur->next;
+            p->next = new tnode;
+            p->next->footbolist = f;
+            p->next->next = cur;
+            return true;
+        }
+        else cur = cur->next;
+    }
+    return false;
+}
+/////////////////////////////////////////////////////////////////////
 
 void free(pnode p) {
     if (p != NULL) {
@@ -133,6 +204,22 @@ void free(pnode p) {
     }
 }
 
+/////////////////////////////////////////////////////////////////////
+bool SameFootbolist(Footbolist footbolist1, Footbolist footbolist2) {
+    return (&footbolist1)->surname == (&footbolist2)->surname  &&
+        (&footbolist1)->age == (&footbolist2)->age &&
+        (&footbolist1)->games_count == (&footbolist2)->games_count &&
+        (&footbolist1)->goal_count == (&footbolist2)->goal_count;
+}
+pnode FindFootbolist(pnode p, Footbolist footbolist0) {
+    while (p != NULL) {
+        if (SameFootbolist(p->footbolist, footbolist0)) {
+            return p;
+        }
+        p = p->next;
+    }
+    return NULL;
+}
 /////////////////////////////////////////////////////////////////////
 
 void PrintFootbolist(pnode footbolist) {
@@ -198,19 +285,22 @@ int main()
                 else cout << "Unknown command" << endl;
             }
             else if (command == 2) {
-                if (footbolist == NULL) {
-                    cout << "n must be bigger than 0" << endl;
-                    continue;
-                }
                 int i = 0;
                 cout << "0 for help" << endl;
                 while (true) {
+                    if (footbolist == NULL) {
+                        cout << "n must be bigger than 0" << endl;
+                        break;
+                    }
                     cin >> command;
-                    if (command == 0) cout << "0 - help\n1 - find best forward\n2 - show all less than 5 games footbolist\n3 - print all\n4 - go back" << endl;
+                    if (command == 0) cout << "0 - help\n1 - find best forward\n2 - show all less than 5 games footbolist\n3 - remove footbolist\n4 - add footbolist before other\n5 - add footbolist after other\n6 - print all\n7 - go back" << endl;
                     else if (command == 1) PrintFootbolist(BestForward(footbolist, footbolist));
                     else if (command == 2) LessThanFiveGames(footbolist);
-                    else if (command == 3) PrintAll(footbolist);
-                    else if (command == 4) break;
+                    else if (command == 3) if (!deleteNode(&footbolist, FindFootbolist(footbolist, SetFootbolist()))) cout << "Error!Requested footbolist can't be found" << endl; else {}
+                    else if (command == 4) if (!insertBefore(&footbolist, FindFootbolist(footbolist, SetFootbolist()), SetOtherFootbolist())) cout << "Error!Requested footbolist can't be found" << endl; else {}
+                    else if (command == 5) if (!insertAfter(&footbolist, FindFootbolist(footbolist, SetFootbolist()), SetOtherFootbolist())) cout << "Error!Requested footbolist can't be found" << endl; else{}
+                    else if (command == 6) PrintAll(footbolist);
+                    else if (command == 7) break;
                     else cout << "Unknown command" << endl;
                 }
                 
