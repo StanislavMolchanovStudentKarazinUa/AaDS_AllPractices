bd98260d4b25413d249b63ffabe814a77f0a7714
StanislavMolchanovStudentKarazinUa <144453439+StanislavMolchanovStudentKarazinUa@users.noreply.github.com>
2023-10-29 17:55:27 +0100

    Update practice6.cpp : Progress#002
    Added struct t_tree
    Added function formTree()
    Added function freeTree()

--- practice6.cpp
+++ practice6.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct t_tree {
+    int info;
+    t_tree* left;
+    t_tree* right;
+};
+typedef t_tree* ptree;
+
+ptree formTree()
+{
+    ptree t;
+    string info;
+    cin >> info;
+    if (info == "x")
+        return NULL;
+    t = new t_tree;
+    t->info = stoi(info);
+    t->left = formTree();
+    t->right = formTree();
+    return t;
+}
+void freeTree(ptree t) {
+    if (t != NULL) {
+        freeTree(t->left);
+        freeTree(t->right);
+        delete t;
+    }
+}
 
 int main(){
 
