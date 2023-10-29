a6bc31c46d2b3bc6c2a3014bbb70958ac26a3e5d
StanislavMolchanovStudentKarazinUa <144453439+StanislavMolchanovStudentKarazinUa@users.noreply.github.com>
2023-10-29 18:03:21 +0100

    Made practice6.cpp
    Filled main() function
    Added ability to use all functions

--- practice6.cpp
+++ practice6.cpp
@@ -66,5 +66,38 @@ int amountOfEqual(ptree tree, int info) {
 }
 
 int main(){
-
+    ptree t = NULL;
+    string command;
+    int info;
+    while (true) {
+        cout << "0 for help" << endl;
+        cin >> command;
+        if (command == "0") cout << "0 - help\n1 - new tree\n2 - continue with existing\nx - exit" << endl;
+        else if (command == "1") {
+            freeTree(t);
+            cout << "Imput elements (x - end of branch): ";
+            t = formTree();
+        }
+        else if (command == "2") {
+            while (true) {
+                cout << "0 for help" << endl;
+                cin >> command;
+                if (command == "0") cout << "0 - help\n1 - display tree TopLeftRight\n2 - display tree LeftTopRight\n3 - display tree LeftRightTop\n4 - amount of equal to definite number\n5 - go back\nx - exit" << endl;
+                else if (command == "1") { displayTLR(t); cout << endl; }
+                else if (command == "2") { displayLTR(t); cout << endl; }
+                else if (command == "3") { displayLRT(t); cout << endl; }
+                else if (command == "4") {
+                    cout << "Enter the number to compare: ";
+                    cin >> info;
+                    cout << amountOfEqual(t, info) << endl;
+                }
+                else if (command == "5") break;
+                else if (command != "x") cout << "Unknown command" << endl;
+                if (command == "x") break;
+            }
+        }
+        else if (command != "x") cout << "Unknown command" << endl;
+        if (command == "x") break;
+    }
+    freeTree(t);
 }
